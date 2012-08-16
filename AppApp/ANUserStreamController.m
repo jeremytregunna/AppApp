448b3a3934836c63164c2aee8b5a53289b8d90e4
Nick Pannuto <sneakyness@sneakyness.com>
2012-08-15 23:38:44 -0700

    API Stuff/Endless Scrolling
    Added GetUserPostsSince/Before ezmode, Added everything I forgot for mentions. Implemented endless scrolling on the other 3 views.

--- AppApp/ANUserStreamController.m
+++ AppApp/ANUserStreamController.m
@@ -37,15 +37,47 @@
 
 - (void)addItemsOnBottom
 {
-    //    [self.tableView reloadData];
-    //
-    //    if (items.count > 50)
-    //        self.canLoadMore = NO; // signal that there won't be any more items to load
-    //    else
-    //        self.canLoadMore = YES;
+    // grab the last post
+    id lastPost = [streamData lastObject];
     
-    // Inform STableViewController that we have finished loading more items
-    [self loadMoreCompleted];
+    // if we have a post
+    if (lastPost) {
+        
+        // fetch old data
+        [[ANAPICall sharedAppAPI] getUserStreamBeforePost:[lastPost objectForKey:@"id"] withCompletionBlock:^(id dataObject, NSError *error) {
+            
+            // verify object
+            if ([dataObject isKindOfClass:[NSArray class]])
+            {
+                // begin updates on table
+                [self.tableView beginUpdates];
+                
+                // get start indexpath
+                NSUInteger startIndexPathRow = [streamData count];
+                NSUInteger endIndexPathRow = [dataObject count] + startIndexPathRow;
+                
+                // add data
+                [streamData addObjectsFromArray:dataObject];
+                
+                // initialize indexpaths
+                NSMutableArray *indexPaths = [[NSMutableArray alloc] initWithCapacity:[dataObject count]];
+                
+                // create array of index paths
+                while (startIndexPathRow < endIndexPathRow) {
+                    [indexPaths addObject:[NSIndexPath indexPathForRow:startIndexPathRow inSection:0]];
+                    startIndexPathRow++;
+                }
+                
+                // insert rows
+                [self.tableView insertRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationAutomatic];
+                [self.tableView endUpdates];
+            }
+            
+            [self loadMoreCompleted];
+        }];
+    } else {
+        [self loadMoreCompleted];
+    }
 }
 
 @end
