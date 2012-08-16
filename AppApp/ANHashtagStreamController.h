5ba0737695a06f4a35e95343042b2fa08fa93c2b
brandon <brandon@redbook.local>
2012-08-16 02:21:32 -0700

    hashtag controller.

---
+++ AppApp/ANHashtagStreamController.h
@@ -0,0 +1,13 @@
+//
+//  ANHashtagStreamController.h
+//  AppApp
+//
+//  Created by brandon on 8/16/12.
+//  Copyright (c) 2012 Sneakyness. All rights reserved.
+//
+
+#import "ANBaseStreamController.h"
+
+@interface ANHashtagStreamController : ANBaseStreamController
+
+@end
