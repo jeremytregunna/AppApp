a5f3f1841c7f1f2066ee0e7bfd124406b62777d1
Johnnie Pittman <jpittman@group6.net>
2012-08-17 13:00:06 -0700

    Added a method to validate whether an auth token is valid.  This does so by checking whether we can return information about the current user from the api.

--- ANAPICall.m
+++ ANAPICall.m
@@ -47,6 +47,61 @@
     return NO;
 }
 
+
+// validates whether the token is valid by requesting info about our user.
+- (BOOL)isAccessTokenValid
+{
+    // our variable for an answer:
+    BOOL answer;
+
+    
+    // Building a url request to handle "get user" query.  The method apart is
+    // async and we need to get this info before we take any other action.
+    NSString *urlString = [NSString stringWithFormat:@"http://alpha-api.app.net/stream/0/users/%@?access_token=%@", self.userID, accessToken];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    // filling in the blanks for the things we need to make this request
+    // synchronous.
+    NSError *error;
+    NSURLResponse *response;
+    
+    // request the data and check to see if we have an error.
+    NSData *json = [NSURLConnection sendSynchronousRequest:request
+                                         returningResponse:&response
+                                                     error:&error];
+    
+    // TODO: Implement better error handling here.
+    if (error) {
+        NSLog(@"error: %@", error);
+    }
+    
+    // Process the data into a dictionary of json data and grab the error code
+    // component if it exists.
+    NSDictionary *data = [NSJSONSerialization JSONObjectWithData:json
+                                                         options:NSJSONReadingAllowFragments
+                                                           error:&error];
+    NSNumber *code = [[data objectForKey:@"error"] objectForKey:@"code"];
+    
+    // validate the code if it exists
+    if ([code isEqualToNumber:[NSNumber numberWithInt:401]]) {
+        
+        // got the unauthorized client code.  kill the access token and return
+        // NO as the answer.
+        accessToken = nil;
+        answer = NO;
+        
+    } else {
+        
+        // we're cool.  return YES
+        answer = YES;
+        
+    }
+    
+    return answer;
+}
+
+
 // TODO: redo these later..
 - (void)readTokenFromDefaults
 {
