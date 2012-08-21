206cccd4413dd7782746c76163e60238a3551752
Ralf Rottmann <ralf@24z.me>
2012-08-21 21:55:17 +0200

    Switched to https...

--- AppApp/Apns/RRConstants.h
+++ AppApp/Apns/RRConstants.h
@@ -5,7 +5,7 @@
 //  Created by Ralf Rottmann on 21.08.12.
 //
 
-#define RR_PLATFORM_URL @"http://my.pushmebaby.com/api/v1"
+#define RR_PLATFORM_URL @"https://my.pushmebaby.com/api/v1"
 #define PMB__API_KEY @"Qm4we99aQgUM2ExYHakT"
 #define PMB_API_SECRET @"D0F6VWUuhB0BrwXIOJxK"
 
