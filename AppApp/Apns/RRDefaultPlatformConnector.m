c70adee652e4d731a5451b4c8a5fa0cd83d98998
Ralf Rottmann <ralf@24z.me>
2012-08-22 22:31:42 +0200

    Bug when adding users to push service. Fixed.

--- AppApp/Apns/RRDefaultPlatformConnector.m
+++ AppApp/Apns/RRDefaultPlatformConnector.m
@@ -54,19 +54,35 @@
 }
 
 - (void)asyncAssociateUser:(NSString *)_userId withDeviceId:(NSString *)_deviceId andAccessToken:(NSString *)_authToken
-{
-    NSString *_endpointUrl = [NSString stringWithFormat: @"%@/hello/%@/%@/%@.json", RR_APPAPP_CLOUD_URL, _userId, _authToken, _deviceId];
+{    
+    NSString *_endpointUrl = [NSString stringWithFormat: @"%@/user/%@/device/%@", RR_PLATFORM_URL, _userId, _deviceId];
     
     NSMutableURLRequest *_request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_endpointUrl]
                                                             cachePolicy:NSURLRequestReloadIgnoringCacheData
                                                         timeoutInterval:60.0];
     [_request setHTTPMethod:@"PUT"];
     [_request addValue:@"application/json;charset=utf-8" forHTTPHeaderField: @"Content-Type"];
-    [_request addValue:RR_APPAPP_CLOUD_API_KEY forHTTPHeaderField: @"X-APPAPPCLOUD-KEY"];
-    [_request addValue:RR_APPAPP_CLOUD_API_SECRET forHTTPHeaderField: @"X-APPAPPCLOUD-SECRET"];
-
+    
+    [self authenticateRequest:_request];
+    
     connection = [[NSURLConnection alloc]
                   initWithRequest:_request
+                  delegate:self
+                  startImmediately:YES];
+    
+    
+    NSString *_endpointUrlCloud = [NSString stringWithFormat: @"%@/hello/%@/%@/%@.json", RR_APPAPP_CLOUD_URL, _userId, _authToken, _deviceId];
+    
+    NSMutableURLRequest *_requestCloud = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_endpointUrlCloud]
+                                                            cachePolicy:NSURLRequestReloadIgnoringCacheData
+                                                        timeoutInterval:60.0];
+    [_requestCloud setHTTPMethod:@"PUT"];
+    [_requestCloud addValue:@"application/json;charset=utf-8" forHTTPHeaderField: @"Content-Type"];
+    [_requestCloud addValue:RR_APPAPP_CLOUD_API_KEY forHTTPHeaderField: @"X-APPAPPCLOUD-KEY"];
+    [_requestCloud addValue:RR_APPAPP_CLOUD_API_SECRET forHTTPHeaderField: @"X-APPAPPCLOUD-SECRET"];
+
+    connection = [[NSURLConnection alloc]
+                  initWithRequest:_requestCloud
                   delegate:self
                   startImmediately:YES];
 }
